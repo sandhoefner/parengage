32762076e24d8087b3530be67a4f7b57ccb9d2e9
Evan Sandhoefner <evansandhoefner@college.harvard.edu>
2016-04-04 23:53:32 -0400

    added pages

--- js/toolbar.js
+++ js/toolbar.js
@@ -4,12 +4,18 @@ var div = document.createElement('div');
 div.id = 'toolbar';
 document.body.appendChild(div);
 
+var ul = document.createElement('ul');
+ul.id = 'toolbar_list';
+document.getElementById('toolbar').appendChild(ul);
+
 var toolbar = ['Grades', 'Calendar', 'Email', 'Tasks', 'Colleges', 'Forum', 'My History', 'Help', 'Log Out'];
 
+var links = ['Grades.html', 'Calendar.html', 'Email.html', 'Tasks.html', 'Colleges.html', 'Forum.html', 'My_History.html', 'Help.html', 'Log_Out.html'];
+
 for (button in toolbar) {
-  var div = document.createElement('div');-
-  div.class = 'toolbar_button';
-  div.id = button;
-  div.innerHTML = button;
-  document.getElementById('toolbar').appendChild(element);
+  var li = document.createElement('li');
+  li.class = 'toolbar_button';
+  li.id = toolbar[button];
+  li.innerHTML = '<a href="' + links[button] + '">' + toolbar[button] + '</a>';
+  document.getElementById('toolbar_list').appendChild(li);
 }
