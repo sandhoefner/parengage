d015f79fcb65a6a34d97aa108e6f68219a5d39de
Jeffrey King <nerdpride4life@gmail.com>
2016-04-06 16:57:50 -0400

    Add glyphicon icons to navbar

--- js/toolbar.js
+++ js/toolbar.js
@@ -1,7 +1,7 @@
 // add toolbar to page
 
 var div = document.createElement('div');
-div.className = 'sidebar-wrapper col-md-2 col-sm-2 col-xs-3'
+div.className = 'sidebar-wrapper col-md-3 col-sm-3 col-xs-4'
 div.id = 'toolbar';
 containerDiv = document.getElementById("content");
 document.body.insertBefore(div, containerDiv)
@@ -13,13 +13,14 @@ ul.id = 'toolbar_list';
 document.getElementById('toolbar').appendChild(ul);
 
 var toolbar = ['Grades', 'Calendar', 'Email', 'Tasks', 'Colleges', 'Forum', 'My History', 'Help', 'Log Out'];
-
+var glyphicons = ['book', 'calendar', 'envelope', 'check', 'globe', 'comment', 'tasks', 'question-sign', 'log-out']
 var links = ['index.html', 'calendar.html', 'email.html', 'tasks.html', 'colleges.html', 'forum.html', 'my_history.html', 'help.html', 'login.html'];
 
 for (button in toolbar) {
   var li = document.createElement('li');
   li.className = 'toolbar_button';
   li.id = toolbar[button];
-  li.innerHTML = '<a href="' + links[button] + '">' + toolbar[button] + '</a>';
+  li.innerHTML = '<a href="' + links[button] + '">' + '<span class="glyphicon glyphicon-' + glyphicons[button] + ' aria-hidden="true"></span>'+ toolbar[button] + '</a>';
+
   document.getElementById('toolbar_list').appendChild(li);
 }
