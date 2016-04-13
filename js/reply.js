427e478a0a37f6778696d1b3e18cda679824650f
franshap <frances.sari@gmail.com>
2016-04-13 16:24:49 -0400

    forum

---
+++ js/reply.js
@@ -0,0 +1,25 @@
+//reply to post
+
+var message = "";
+var replybutton = "<br><button class='btn btn-default btn-xs reply'>Reply</button>";
+
+$(document).ready(function() {
+	sendreply();
+})
+
+var sendreply = function() {
+	$(".reply").click(function() {
+		$(this).replaceWith("<div><textarea class='form-control message' type='text' name='email_body' placeholder='Message'></textarea><br><button class='btn btn-default btn-xs sendreply' type='submit'>Send Reply</button></div>"); 
+
+
+	$(".sendreply").click(function() {
+		message = $(this).parent().children(".message").val();
+		var time = new Date();
+		var responsetime= (time.getMonth()+1) + '/' + time.getDate() + '/' + time.getFullYear() + ' ' + time.getHours() + ':' + time.getMinutes();
+		$(this).parent().replaceWith("<div><br>Username at " + responsetime + ":<br>\"" + message + "\"<br></div>" + replybutton); 
+
+		sendreply();
+	});
+
+	});
+}
