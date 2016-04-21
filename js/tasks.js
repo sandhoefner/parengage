a697135ed1e6b9f2735c61aea9118e4a2145cab1
Evan Sandhoefner <evansandhoefner@gmail.com>
2016-04-20 20:32:59 -0400

    fixed jquery

--- js/tasks.js
+++ js/tasks.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	// document.onload = function() {
 	// Increments up every time a task is created. Would be later be taken from JSON
 	$(".addTask").click(function(){
 		addTaskForm();
@@ -11,7 +12,7 @@ $(document).ready(function(){
 	$(function() {
     	$( "#datepicker" ).datepicker();
   	});*/
-})
+
 
 /*
 *
@@ -40,7 +41,7 @@ var saveForm = function () {
 	taskDetails = $(".taskDetails").val();
 
 	// Create html and append
-	html = '<div class="task" id="task' + taskNumber + '">';
+	html = '<div class="task" id="' + taskName + '">';
 	html += '<h3>' + taskName + '</h3>';
 	html += '<h5>' + taskDate + '</h5>';
 	var split = taskDetails.split('\n');
@@ -62,9 +63,9 @@ var saveForm = function () {
 	taskNumber += 1;
 }
 
-function init(name, body, list) {
+var init = function(name, body, list) {
 	// Create html and append
-	html = '<div class="task" id="task' + taskNumber + '">';
+	html = '<div class="task" id="' + name + '">';
 	html += '<h3>' + name + '</h3>';
 	html += '<h5>' + body + '</h5>';
 	html += "<div class='checkbox'>";
@@ -74,13 +75,54 @@ function init(name, body, list) {
 	html += "</div>";
 	html += '<p><a class="clickable" onclick="dele(this.parentElement.parentElement);">Delete</a></p>';
 	html += '</div>';
-	document.getElementById("taskWrapper").innerHTML += html;
 
+	// var total_html = (html + document.getElementById("taskWrapper").innerHTML) || html;
+	// document.getElementById("taskWrapper").innerHTML += html;
+	// document.getElementById("taskWrapper").innerHTML = total_html;
+$(".taskWrapper").append(html);
 	taskNumber += 1;
 }
-
-function dele(elt) {
+var dele = function(elt) {
 	taskNumber -= 1;
+
+	                // localStorage["tasks"] = JSON.stringify(tasks);
+for (var i = 0; i < keys.length; i++) {
+            if (notes[i] == elt.id) {
+                notes.splice(i, 1);
+                localStorage["keys"] = JSON.stringify(keys);
+                break;
+            }
+        }
+        // localStorage.keys = keys;
 	elt.remove();
 }
 
+// first time user
+if (!localStorage["returning_user"]) {
+localStorage.keys = JSON.stringify(["Bake Sale!"]);
+localStorage["Bake Sale!"] = JSON.stringify(["If you have a moment, please call a few of these parents and ask them to volunteer for the bake sale! If you do call anyone, please check their box and press 'submit' so we don't call them again.", "Pam Davis: 555-555-5555", "Jim Clooney: 555-555-5555", "Martha Kim: 555-555-5555", "Lily Kerr: 555-555-5555"]);
+init("Bake Sale!", "If you have a moment, please call a few of these parents and ask them to volunteer for the bake sale! If you do call anyone, please check their box and press 'submit' so we don't call them again.", ["Pam Davis: 555-555-5555", "Jim Clooney: 555-555-5555", "Martha Kim: 555-555-5555", "Lily Kerr: 555-555-5555"]);
+} else {
+// returning user
+localStorage.returning_user = "1";
+keys = JSON.parse(localStorage.keys);
+for (i=0;i<keys.length;i++) {
+	var key = keys[i];
+	var array = JSON.parse(localStorage[key]);
+	var desc = array[0];
+	array.shift();
+	init(key, desc,  array);
+}
+}
+
+// // first time user
+//     if (!localStorage["tasks"]) {
+
+//         var tasks = "Bake Sale!", "If you have a moment, please call a few of these parents and ask them to volunteer for the bake sale! If you do call anyone, please check their box and press 'submit' so we don't call them again.", ["Pam Davis: 555-555-5555", "Jim Clooney: 555-555-5555", "Martha Kim: 555-555-5555", "Lily Kerr: 555-555-5555"]]];
+
+//         localStorage.tasks = tasks;
+//     // returning user
+// } else {
+// tasks = JSON.parse(localStorage["tasks"]);
+// }
+});
